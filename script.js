7ded9371ff5b587c55239e1749d235449e2818a6
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-16 12:04:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function showURLLength() {
+    const url = location.href;   // get current URL
+    const len = url.length;      // length of URL
+    alert("The length of the URL is: " + len);
+}
