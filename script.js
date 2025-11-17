e38e3bec694e2324e4561b75bcecaeda4e243c9a
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-17 22:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,34 +3,29 @@ const inputs = document.querySelectorAll(".code");
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
-    
-    input.addEventListener("input", (e) => {
-        const value = e.target.value;
 
-        // Only allow digits
-        if (!/^[0-9]$/.test(value)) {
-            e.target.value = "";
+    input.addEventListener("input", (e) => {
+        const val = e.target.value;
+
+        if (!/^[0-9]$/.test(val)) {
+            input.value = "";
             return;
         }
 
-        // Move to next box if exists
-        if (value !== "" && index < inputs.length - 1) {
+        if (index < inputs.length - 1) {
             inputs[index + 1].focus();
         }
     });
 
     input.addEventListener("keydown", (e) => {
-
-        // Handle Backspace Key
         if (e.key === "Backspace") {
+
             if (input.value === "") {
-                // go to previous field
                 if (index > 0) {
                     inputs[index - 1].focus();
                     inputs[index - 1].value = "";
                 }
             } else {
-                // Delete current digit
                 input.value = "";
             }
         }
