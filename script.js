3db29756b0f03d1f544aa1c4b6eb404068bdc38b
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-17 22:05:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-//your JS code here. If required.
-function showURLLength() {
-    const url = location.href;   // get current URL
-    const len = url.length;      // length of URL
-    alert("The length of the URL is: " + len);
-}
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+    
+    input.addEventListener("input", (e) => {
+        const value = e.target.value;
+
+        // Only allow digits
+        if (!/^[0-9]$/.test(value)) {
+            e.target.value = "";
+            return;
+        }
+
+        // Move to next box if exists
+        if (value !== "" && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+
+        // Handle Backspace Key
+        if (e.key === "Backspace") {
+            if (input.value === "") {
+                // go to previous field
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                    inputs[index - 1].value = "";
+                }
+            } else {
+                // Delete current digit
+                input.value = "";
+            }
+        }
+    });
+
+});
