bbdeacc01f442ffaf4cba4e3bcd3eee065424f82
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-19 09:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const inputs = document.querySelectorAll(".code");
 
+// Focus first input
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
-  
-  input.addEventListener("input", () => {
-    let value = input.value;
 
-    // Accept only digits
-    input.value = value.replace(/\D/g, "");
+  input.addEventListener("input", (e) => {
+    const value = e.target.value.replace(/\D/g, ""); // only digits
+    e.target.value = value;
 
-    if (input.value && index < inputs.length - 1) {
+    if (value && index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      if (input.value === "" && index > 0) {
-        inputs[index - 1].focus();
-        inputs[index - 1].value = "";
+      if (input.value === "") {
+        // current empty → go to previous
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = "";
+        }
+      } else {
+        // current has value → clear it
+        input.value = "";
       }
     }
   });
+
 });
