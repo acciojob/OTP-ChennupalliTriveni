123226c2af62cbd7794712baf51156e789e37069
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-27 11:49:38 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll(".otp-inputs .otp-input");
+
+inputs.forEach((input, index) => {
+
+    input.addEventListener("input", () => {
+        if (input.value && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && !input.value && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
