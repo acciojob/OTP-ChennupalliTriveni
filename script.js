7b295df15521bb90980b032af32dcdbb8bab6f19
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-27 12:01:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const inputs = document.querySelectorAll(".otp-inputs .otp-input");
+const inputs = document.querySelectorAll(".code-container .code");
 
 inputs.forEach((input, index) => {
 
     input.addEventListener("input", () => {
-        if (input.value && index < inputs.length - 1) {
+        
+        // Allow only digits
+        input.value = input.value.replace(/[^0-9]/g, "");
+
+        // Move to next input if current has a value
+        if (input.value !== "" && index < inputs.length - 1) {
             inputs[index + 1].focus();
         }
     });
 
     input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace" && !input.value && index > 0) {
+
+        // Backspace: move to previous input if current is empty
+        if (e.key === "Backspace" && input.value === "" && index > 0) {
             inputs[index - 1].focus();
-        }
+        }
     });
+
 });
