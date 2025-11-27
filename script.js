46cdbf1944e170be3e5a4e3fe72964faa0496308
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-27 11:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-const inputs = document.querySelectorAll(".code");
+const inputs = document.querySelectorAll(".otp-inputs input");
 
-// Auto move + only numbers allowed
 inputs.forEach((input, index) => {
 
-  input.addEventListener("input", () => {
-    input.value = input.value.replace(/[^0-9]/g, "");
+    // Move forward automatically
+    input.addEventListener("input", () => {
+        if (input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
 
-    if (input.value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-    }
-  });
+    // Move backward using Backspace
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && input.value === "" && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
 
-  // Backspace → previous input
-  input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && !input.value && index > 0) {
-      inputs[index - 1].focus();
-    }
-  });
-});
-
-// Paste complete OTP
-inputs[0].addEventListener("paste", (e) => {
-  const text = e.clipboardData.getData("text").replace(/\D/g, "");
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].value = text[i] || "";
-  }
 });
