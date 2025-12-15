8fb24cdc9f2cb5c6b96e817c2ade5ca5cd5de790
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-12-15 20:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const inputs = document.querySelectorAll(".otp-inputs .otp-input");
+const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
 
-    input.addEventListener("input", () => {
+  input.addEventListener("input", () => {
+    input.value = input.value.replace(/[^0-9]/g, "");
 
-        // Only digits allowed
-        input.value = input.value.replace(/[^0-9]/g, "");
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
 
-        if (input.value && index < inputs.length - 1) {
-            inputs[index + 1].focus();
-        }
-    });
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      } else {
+        input.value = "";
+      }
+    }
+  });
 
-    input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace" && !input.value && index > 0) {
-            inputs[index - 1].focus();
-        }
-    });
 });
