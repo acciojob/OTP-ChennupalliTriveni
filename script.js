9767a70c334bac62d1deb605866e88856dae90ec
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-12-15 20:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ inputs.forEach((input, index) => {
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      if (input.value === "" && index > 0) {
-        inputs[index - 1].focus();
-        inputs[index - 1].value = "";
+
+      if (input.value === "") {
+        if (index > 0) {
+          inputs[index - 1].value = "";
+          inputs[index - 1].focus();
+        }
       } else {
         input.value = "";
       }
